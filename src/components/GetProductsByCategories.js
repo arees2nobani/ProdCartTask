eeb25c4422c42ae3b44806f739fcfabcc4c2726b
arees2nobani <areesnobani@gmail.com>
2024-07-22 10:46:21 +0300

    added search

--- src/components/GetProductsByCategories.js
+++ src/components/GetProductsByCategories.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 const TruncatedText = ({ text, maxLength }) => {
   const [isTruncated, setIsTruncated] = useState(true);
@@ -13,8 +14,8 @@ const TruncatedText = ({ text, maxLength }) => {
     <p>
       {displayText}
       {text.length > maxLength && (
-        <span onClick={toggleTruncation} style={{ color: 'blue', cursor: 'pointer', fontSize: '1.4rem' }}>
-          {isTruncated ? '... (see more)' : ' (see less)'}
+        <span onClick={toggleTruncation} style={{ color: 'blue', cursor: 'pointer', fontSize: '1.0rem' }}>
+          {isTruncated ? ' ...see more' : ' ...see less'}
         </span>
       )}
     </p>
@@ -27,6 +28,9 @@ function GetProductByCategory() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [categories, setCategories] = useState([]);
+  const [query, setQuery] = useState('');
+
+
 
   useEffect(() => {
     fetch('https://dummyjson.com/products/categories')
@@ -65,6 +69,7 @@ function GetProductByCategory() {
   }, [productCategory]);
 
   const handleAddToCart = (productId, quantity = 1) => {
+    
     fetch('https://dummyjson.com/carts/add', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -81,28 +86,81 @@ function GetProductByCategory() {
     .then(res => res.json())
     .then(data => {
       console.log('Added to cart:', data);
-      alert('added to cart')
 
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "added to Cart!",
+          showConfirmButton: false,
+          timer: 1300
+        });   
+      
     })
     .catch(err => {
       console.error('Error adding to cart:', err);
     });
   };
+  //search
+
+  const searchProducts = () => {
+    setLoading(true);
+    setError(null);
+    fetch(`https://dummyjson.com/products/search?q=${query}`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('not found');
+        }
+        return res.json();
+      })
+      .then(data => {
+        setProducts(data.products);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError(err);
+        setLoading(false);
+      });
+  };
+
+const handleKeyPress = (e) => { // Added function to handle key press
+  if (e.key === 'Enter') {
+    searchProducts();
+  }
+};
+/////////////////////////////
 
 
 
   return (
     <div className='Selection'>
-      <h2>MyShop</h2>
+      <div className='divHeader'>
+      <i><h1>MyShop</h1></i>
+      
       <select
         value={productCategory}
         onChange={(e) => setProductCategory(e.target.value)}
+        className='selectCSS'
       >
         <option value="" disabled>Category</option>
         {categories.map((category, index) => (
           <option key={index} value={category.slug}>{category.name}</option>
         ))}
       </select>
+      
+      {/* search sort (by price arc or desc)  add cart to see the added products and they can be deleted from it */}
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="🔍 Search.. "
+        className='inputOfSearch'
+        onKeyPress={handleKeyPress}
+      />
+      
+      </div> 
+
+      <br />      
+      <br />
 
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
